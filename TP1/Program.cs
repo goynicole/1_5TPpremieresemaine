298726f58cdbc7fccce5b020bc88ed587af9d964
goy <goy.nicole018@gmail.com>
2019-01-02 11:26:33 +0100

    TP

---
+++ TP1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float P, T, IMC;//float pour indiquer que l'on va rentrer des nombres à virgule
+
+            Console.WriteLine("Quel est votre poids?");
+            P = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quel est votre taille?");
+            T = float.Parse(Console.ReadLine());
+
+            IMC = (P / (T*T)); //IMC = Poids divisé par la taille x 2 (Poids/Taille*2)
+
+            if (IMC < 16.5)
+            {
+                Console.WriteLine("Vous êtes en Dénutrition");
+            }
+            else if ((IMC > 16.5) && (IMC < 18.5))
+            {
+                Console.WriteLine("Vous êtes en Maigreur");
+            }
+            else if ((IMC > 18.5) && (IMC < 25))
+            {
+                Console.WriteLine("Vous êtes en Corpulence normale");
+            }
+            else if ((IMC > 25)&&(IMC < 30))
+            {
+                Console.WriteLine("Vous êtes en Surpoids");
+            }
+            else if ((IMC > 30)&&(IMC < 35))
+            {
+                Console.WriteLine("Vous êtes en Obésité Modérée");
+            }
+            else if ((IMC > 35)&&(IMC < 40))
+            {
+                Console.WriteLine("Vous êtes en Obésité sévère");
+            }
+            else if (IMC > 40)
+            {
+                Console.WriteLine("Vous êtes en Obésité morbide ou massive");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TP2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valeurADeviner = new Random().Next(1, 50);//random représente un générateur de nombres pseudo-aléatoires
+            int nombreDeCoups = 0;
+            bool devine = false;
+            Console.WriteLine("TROUVE LE NOMBRE! Il est compris entre 1 et 50");
+            while (!devine)
+            {
+                String saisie = Console.ReadLine();
+                int valeurSaisie = 18;// nombre a trouvé
+                if (int.TryParse(saisie, out valeurADeviner))
+                {
+                    if (valeurSaisie == valeurADeviner)// si le nombre a trouvé est bien tapé l'odi dira que c'est bon 
+                        devine = true;
+                    else
+                    {
+                        if (valeurSaisie > valeurADeviner)
+                            Console.WriteLine("Le nombre à deviner est plus grand que 15");
+                        else
+                            Console.WriteLine("Le nombre est plus petit que 26");
+                    }
+                    nombreDeCoups++;
+                }
+                else
+                    Console.WriteLine("La valeur saisie est incorrecte, RECOMMENCE!");
+            }
+            Console.Write("Vous avez trouvé en " + nombreDeCoups + " coup(s)");
+        }
+    }
+}
